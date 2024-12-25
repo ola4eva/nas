035e54df7e88543ca6595fcf7f2a498fb81c672b
Olalekan Babawale <padinality@yahoo.com>
2024-12-25 03:59:50 +0100

    move py files into models package and views into views/ folder

--- account_cash_advance/__manifest__.py
+++ account_cash_advance/__manifest__.py
@@ -1,34 +1,33 @@
-
 {
-    'name' : 'Employee Cash Advances',
-    'version' : '16.0',
-    'depends' : ["base", "hr", 'hr_expense', 'hr_contract', 'hr_payroll'],
-    'author' : 'Mattobell',
-    'website' : 'http://www.mattobell.com',
-    'description': '''
+    "name": "Employee Cash Advances",
+    "version": "0.0.1",
+    "author": "Olalekan Babawale",
+    "website": "http://obabawale.github.io",
+    "description": """
 Management of Various Cash Advances to Employees
-============================
+================================================
 
 Features
 --------
 * Salary Advances and Payment
 * Pretty Cash Advances for Expense and Retirements
-    ''',
-    'category' : 'Accounting & Finance',
-    'sequence': 70,
-    'data' : [
-        'security/account_salary_security.xml',
-        'security/ir.model.access.csv',
-        'report/cash_advance_report_reg.xml',
+    """,
+    "category": "Accounting & Finance",
+    "sequence": 70,
+    "depends": ["base", "accountant", "hr", "hr_expense", "hr_contract", "hr_payroll"],
+    "data": [
+        "security/account_salary_security.xml",
+        "security/ir.model.access.csv",
         'report/cash_advance_report_view.xml',
-        'company_view.xml',
-        'ng_account_cash_view.xml',
-        'ng_account_expense_view.xml',
-        'hr_expense_view.xml',
-        'ng_refund_advance_view.xml',
-#        'report.xml',
+        'views/res_company_view.xml',
+        'views/cash_advance_view.xml',
+        'views/ng_account_cash_view.xml',
+        'views/hr_employee_view.xml',
+        'views/hr_expense_retirement_view.xml',
+        'views/ng_refund_advance_view.xml',
     ],
-    'auto_install': False,
-    'installable': True,
-    'application': False,
+    "auto_install": False,
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
 }
