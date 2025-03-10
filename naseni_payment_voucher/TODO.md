c858078d8122a1b1648cc553f9122aa8820cc42a
Olalekan Babawale <padinality@yahoo.com>
2025-03-10 10:51:57 +0100

    [ADD] naseni_payment_voucher: add payment voucher module

---
+++ naseni_payment_voucher/TODO.md
@@ -0,0 +1,7 @@
+## Naseni Payment Voucher
+## -------------------------
+
+### Todo
+- [] rename budget to Votebook
+- [] move budget menu to the dashboard / landing page
+- []
---
+++ naseni_payment_voucher/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Payment Voucher",
+    'summary': "Payment Voucher Implementation",
+    'description': """
+        Payment Voucher Implementation
+    """,
+    'author': "HyperIT Consultants",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['account_budget'],
+    'license': "LGPL-3",
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/votebook_views.xml',
+    ],
+}
+
