b032939555568ee7867f33f4cac4f1b2af0f3f78
Olalekan Babawale <padinality@yahoo.com>
2025-03-14 13:18:49 +0100

    [ADD] naseni_store_request: add store requisition module

---
+++ naseni_store_request/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Store Request",
+
+    'summary': """
+        Topline store request""",
+
+    'description': """
+        topline store request
+    """,
+
+    'author': "HyperIT Consultants",
+    'website': "https://www.yourcompany.com",
+
+    'category': 'Inventory',
+    'version': '0.1',
+
+    'license': 'LGPL-3',
+
+    'depends': [
+        'stock',
+        'hr',
+        'project',
+        # 'base_extension',
+        # 'topline',
+    ],
+
+    'data': [
+        # 'data/ir_sequence.xml',
+        # 'data/store_request_data.xml',
+        # 'security/ir.model.access.csv',
+        # 'security/store_request_security.xml',
+        # 'views/stock_move_views.xml',
+        # 'views/stock_picking_rejection_views.xml',
+        # 'views/stock_picking_views.xml',
+    ],
+}
---
+++ naseni_store_request/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import brand_type
+from . import stock_move
+from . import stock_picking
+from . import stock_rejection
