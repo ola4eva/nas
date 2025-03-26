f1a411033d002d287a1a4a318adc19558de23877
Olalekan Babawale <padinality@yahoo.com>
2025-03-26 01:39:09 +0100

    Add payment voucher pdf report code

--- naseni_payment_voucher/__manifest__.py
+++ naseni_payment_voucher/__manifest__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Payment Voucher",
-    'summary': "Payment Voucher Implementation",
-    'description': """
+    "name": "Payment Voucher",
+    "summary": "Payment Voucher Implementation",
+    "description": """
         Payment Voucher Implementation
     """,
-    'author': "HyperIT Consultants",
-    'website': "https://www.yourcompany.com",
-    'category': 'Uncategorized',
-    'version': '0.1',
-    'depends': ['account_budget'],
-    'license': "LGPL-3",
-    'data': [
+    "author": "HyperIT Consultants",
+    "website": "https://www.yourcompany.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["account_budget"],
+    "license": "LGPL-3",
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/votebook_views.xml',
+        "views/votebook_views.xml",
+        # "views/payment_voucher_template.xml",
+        # "views/payment_voucher_report.xml",
+        "views/payment_voucher.xml",
     ],
 }
-
