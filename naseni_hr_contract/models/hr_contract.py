1afadf09bbff830fa2e3c5afb4f749d4c892c2af
Olalekan Babawale <padinality@yahoo.com>
2025-03-27 09:22:56 +0100

    [ADD] naseni_hr_contract: add module to update contracts using server action

---
+++ naseni_hr_contract/models/hr_contract.py
@@ -0,0 +1,23 @@
+import logging
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class HrContract(models.Model):
+    _inherit = "hr_contract.contract"
+
+    skip_from_progress_action = fields.Boolean("Skip From Server Action")
+
+    @api.model
+    def start_contracts_except_skipped(self):
+        contracts_to_start = self.search([]).filtered(
+            lambda contract: not contract.skip_from_progress_action
+        )
+        _logger.info(f"Before: Contracts to be set to running... {contracts_to_start}")
+        contracts_to_start and contracts_to_start.update({"state": "open"})
+        contracts_to_start = self.search([]).filtered(
+            lambda contract: not contract.skip_from_progress_action
+        )
+        _logger.info(f"After: Contracts to be set to running... {contracts_to_start}")
+        return True
