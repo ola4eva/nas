6658b3057b438674741ee28819e93a9dae3f84cb
Olalekan Babawale <padinality@yahoo.com>
2025-04-28 18:41:38 +0100

    Add feature to confirm deductions in batch

--- payroll_bonus_deduction/models/payroll_bonus_deduction.py
+++ payroll_bonus_deduction/models/payroll_bonus_deduction.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import date
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class PayrollBonusDeduction(models.Model):
@@ -8,17 +8,27 @@ class PayrollBonusDeduction(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Payroll Bonuses & Deductions'
 
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Name", required=True)
     note = fields.Text('Description')
     employee_id = fields.Many2one(
-        comodel_name="hr.employee", string="Employee")
-    date = fields.Date(string='Date', default=date.today())
-    other_input_id = fields.Many2one('hr.payslip.input.type', string='Other Input')
+        comodel_name="hr.employee", string="Employee", required=True)
+    date = fields.Date(string='Date', default=date.today(), required=True)
+    other_input_id = fields.Many2one('hr.payslip.input.type', string='Other Input', required=True)
     amount = fields.Float('Amount', default=0.0)
     state = fields.Selection([
         ('draft', 'New'),
         ('confirm', 'Confirmed')
     ], string='State', default="draft", readonly="1", tracking=True)
 
-    def action_confirm(self):
+    def _action_confirm(self):
         return self.write({'state': "confirm"})
+
+    def unlink(self):
+        for record in self:
+            if record.state != 'draft':
+                self -= record
+        return super(PayrollBonusDeduction, self).unlink()
+
+    def action_confirm(self):
+        """Confirm entries in batch"""
+        return (self - self.filtered(lambda r: r.state != 'draft'))._action_confirm()
