fce6d804e84e21c2a876957e14e50e1442a21ac4
ola4eva <ola4eva2001@gmail.com>
2025-06-25 22:48:11 +0100

    Update hr_employee.py
    Added employee number and budget line fields

--- naseni_hr/models/hr_employee.py
+++ naseni_hr/models/hr_employee.py
@@ -49,6 +49,8 @@ class HrEmployeeBase(models.AbstractModel):
     tin = fields.Char("Tax ID No.(*)", required=True)
     nhf = fields.Char("National Housing Fund")
     nin = fields.Char("National ID No.")
+    emp_no = fields.Char("Employee No.(*)")
+    budget_line = field.Char("Budget Line")
     title_id = fields.Many2one(comodel_name="res.partner.title", string="Title")
     next_of_kin_ids = fields.One2many('naseni_hr.next_of_kin', 'employee_id', string='Next of Kin')
 
