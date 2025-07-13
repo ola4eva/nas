c0db51017cb316f5b01ab2529a03d5a629b10872
Olalekan Babawale <padinality@yahoo.com>
2025-07-13 13:54:36 +0100

    Display votebook in Payment voucher PDF

--- naseni_account/__manifest__.py
+++ naseni_account/__manifest__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Naseni Account Extension",
-    'summary': "Naseni Account Extension",
-    'description': """
+    "name": "Naseni Account Extension",
+    "summary": "Naseni Account Extension",
+    "description": """
         Naseni Account Extension
     """,
-    'author': "HyperIT Consultants",
-    'website': "https://www.yourcompany.com",
-    'category': 'Uncategorized',
-    'version': '0.1',
-    'depends': ['accountant'],
-    'license': "LGPL-3",
+    "author": "HyperIT Consultants",
+    "website": "https://www.yourcompany.com",
+    "depends": ["accountant"],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+    "category": "Uncategorized",
+    "version": "0.1",
+    "license": "LGPL-3",
 }
-
