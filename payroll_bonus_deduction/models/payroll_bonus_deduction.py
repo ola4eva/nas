3ed9c4f3fa83f2ea7a0c48c7c26099facddcdfd7
Olalekan Babawale <padinality@yahoo.com>
2025-08-12 17:55:55 +0100

    [IMP] payroll_bonus_deduction: change import required field to staff id instead of employee name

--- payroll_bonus_deduction/models/payroll_bonus_deduction.py
+++ payroll_bonus_deduction/models/payroll_bonus_deduction.py
@@ -12,8 +12,8 @@ class PayrollBonusDeduction(models.Model):
     name = fields.Char(string="Name", required=True)
     note = fields.Text('Description')
     employee_id = fields.Many2one(
-        comodel_name="hr.employee", string="Employee", required=True)
-    staff_id = fields.Char('Staff ID')
+        comodel_name="hr.employee", string="Employee", required=False)
+    staff_id = fields.Char('Staff ID', required=True)
     date = fields.Date(string='Date', default=date.today(), required=True)
     other_input_id = fields.Many2one('hr.payslip.input.type', string='Other Input', required=True)
     amount = fields.Float('Amount', default=0.0)
@@ -31,4 +31,27 @@ class PayrollBonusDeduction(models.Model):
     def action_confirm(self):
         """Confirm entries in batch"""
         return (self - self.filtered(lambda r: r.state != 'draft'))._action_confirm()
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'employee_id' not in vals and 'staff_id' in vals:
+                staff_id = vals.get('staff_id')
+                employee = self.env['hr.employee'].search([('staff_id', '=', staff_id)], limit=1)
+                if employee:
+                    vals['employee_id'] = employee.id
+                else:
+                    vals['employee_id'] = False
+        return super().create(vals_list)
+    
+    @api.onchange('staff_id')
+    def _onchange_staff_id(self):
+        print("method _onchange_staff_id called")
+        if self.staff_id:
+            employee = self.env['hr.employee'].search([('staff_id', '=', self.staff_id)], limit=1)
+            print(f"Found employee: {employee.name if employee else 'None'}")
+            if employee:
+                self.employee_id = employee.id
+            else:
+                self.employee_id = False
     
